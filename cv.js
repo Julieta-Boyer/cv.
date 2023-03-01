9c523831b81850ed04f56ff47855a33577ab4251
Julieta-Boyer <126195935+Julieta-Boyer@users.noreply.github.com>
2023-03-01 20:27:09 -0300

    Delete cv.js

--- cv.js
+++
@@ -1,5 +0,0 @@
-
-const datos = ' Mi nombre es Riley Howard, tengo 31 años';
-const email ='jennie.nichols@example.com';
-const direccion = 'Valwood Pkwy 710,Michigan, Estados Unidos';
-const telefono = '(272) 790-0888, celular: (489) 330-2385';
